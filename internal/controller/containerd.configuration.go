100977ac5cebf79cfffa1a8f1563d2b74e173425
irbgeo <morochevgm@gmail.com>
2022-12-30 10:44:12 +0300

    add containerd configuration

---
+++ internal/controller/containerd.configuration.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	_ "embed"
+	"fmt"
+
+	containerd "github.com/containerd/containerd/cmd/containerd/command"
+	"github.com/irbgeo/go-structure"
+	"github.com/pelletier/go-toml"
+
+	"github.com/fraima/fraima/internal/config"
+)
+
+const (
+	containerdConfigurationFilePath = "/etc/kubernetes/containerd/config.toml"
+	containerdConfigurationFilePERM = 0644
+)
+
+// createContainerdConfiguration create containerd.service file.
+func createContainerdConfiguration(cfg config.File) error {
+	data, err := createContainerdConfigurationData(cfg)
+	if err != nil {
+		return err
+	}
+
+	return createFile(containerdConfigurationFilePath, data, containerdConfigurationFilePERM)
+}
+
+func createContainerdConfigurationData(cfg config.File) ([]byte, error) {
+	extraArgs := make(map[string]any)
+	if cfg.ExtraArgs != nil {
+		args, ok := cfg.ExtraArgs.(map[any]any)
+		if !ok {
+			return nil, fmt.Errorf("args converting is not available")
+		}
+		for k, v := range args {
+			extraArgs[fmt.Sprint(k)] = v
+		}
+	}
+
+	st, err := structure.New(new(containerd.Config))
+	if err != nil {
+		return nil, err
+	}
+
+	err = st.AssignFrom(extraArgs)
+	if err != nil {
+		return nil, err
+	}
+
+	data, err := toml.Marshal(st.Struct())
+	fmt.Printf("%s", data)
+	return data, err
+}
