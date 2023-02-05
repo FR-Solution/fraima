b1d92c0dd091288c50255fbdd117400317c41a0e
irbgeo <morochevgm@gmail.com>
2023-02-05 21:39:07 +0400

    steps

--- internal/config/types.go
+++ internal/config/types.go
@@ -3,5 +3,36 @@ package config
 type Instruction struct {
 	APIVersion string `yaml:"apiVersion"`
 	Kind       string `yaml:"kind"`
-	Spec       any    `yaml:"spec"`
+	Spec       Spec   `yaml:"spec"`
+}
+
+type Spec struct {
+	Configuration Config                `yaml:"configuration"`
+	Service       Config                `yaml:"service"`
+	Download      []DownloadInstruction `yaml:"download"`
+	Starting      []string              `yaml:"starting"`
+}
+
+type Config struct {
+	ExtraArgs any `yaml:"extraArgs"`
+}
+
+type DownloadInstruction struct {
+	Name       string    `yaml:"name"`
+	Src        string    `yaml:"src"`
+	CheckSum   *CheckSum `yaml:"checkSum"`
+	HostPath   string    `yaml:"path"`
+	Owner      string    `yaml:"owner"`
+	Permission int       `yaml:"permission"`
+	Unzip      Unzip     `yaml:"unzip"`
+}
+
+type Unzip struct {
+	Status bool     `yaml:"status"`
+	Files  []string `yaml:"files"`
+}
+
+type CheckSum struct {
+	Src  string `yaml:"src"`
+	Type string `yaml:"type"`
 }
--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -1,14 +1,34 @@
 package controller
 
-import "github.com/fraima/fraimactl/internal/config"
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+	"sync"
+
+	"github.com/fraima/fraimactl/internal/config"
+	"go.uber.org/zap"
+)
 
 type generator interface {
-	Run(kind string, instruction GenerateInstruction) error
+	Run(kind, fileType string, extraArgs any) error
 }
 
 type downloader interface {
-	Run(kind string, instruction DownloadInstruction) error
+	Run(instructions []config.DownloadInstruction) error
 }
+
+var (
+	phaseNames []string = []string{
+		"containerd",
+		"kubelet",
+		"modprob",
+		"sysctl",
+	}
+
+	configurationFileType = "configuration"
+	serviceFileType       = "service"
+)
 
 type controller struct {
 	generator  generator
@@ -25,10 +45,52 @@ func New(
 	}
 }
 
-func (s *controller) Run(instructions []config.Instruction) {
+func (s *controller) Run(instructions []config.Instruction, skippingPhases map[string]struct{}) {
+	for _, i := range instructions {
+		if _, isSkipping := skippingPhases[strings.ToLower(i.Kind)]; isSkipping {
+			continue
+		}
 
+		go func(i config.Instruction) {
+			wg := &sync.WaitGroup{}
+
+			wg.Add(3)
+			go s.generation(wg, i.Kind, configurationFileType, i.Spec.Configuration.ExtraArgs)
+			go s.generation(wg, i.Kind, configurationFileType, i.Spec.Service.ExtraArgs)
+			go s.downloading(wg, i.Kind, i.Spec.Download)
+
+			wg.Wait()
+		}(i)
+	}
 }
 
-func (s *controller) phasesSplit(instructions []config.Instruction) []any
+func (s *controller) generation(wg *sync.WaitGroup, kind, fileType string, extraArgs any) {
+	defer wg.Done()
+	if err := s.generator.Run(kind, fileType, extraArgs); err != nil {
+		zap.L().Error("generation", zap.String("kind", kind), zap.String("type", fileType), zap.Error(err))
+	}
+}
 
-func (s *controller) getPhaseName() (string, error)
+func (s *controller) downloading(wg *sync.WaitGroup, kind string, instructions []config.DownloadInstruction) {
+	defer wg.Done()
+	if err := s.downloader.Run(instructions); err != nil {
+		zap.L().Error("downloading", zap.String("kind", kind), zap.Error(err))
+	}
+}
+
+func (s *controller) starting(kind string, instructions []string) error {
+	for _, i := range instructions {
+		commands := strings.Split(i, " ")
+		var err error
+		if len(commands) == 1 {
+			err = exec.Command(commands[0]).Run()
+		} else {
+			err = exec.Command(commands[0], commands[1:]...).Run()
+		}
+
+		if err != nil {
+			return fmt.Errorf("trying execution command: %s", i)
+		}
+	}
+	return nil
+}
