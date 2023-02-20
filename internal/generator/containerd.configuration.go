066d6be59483cdba112ff4ea6df9757be25d3d1a
irbgeo <morochevgm@gmail.com>
2023-02-20 13:44:13 +0400

    fix and test

--- internal/generator/containerd.configuration.go
+++ internal/generator/containerd.configuration.go
@@ -3,10 +3,9 @@ package generator
 import (
 	"bytes"
 	_ "embed"
-	"regexp"
 
-	containerd "github.com/containerd/containerd/services/server/config"
-	"github.com/irbgeo/go-structure"
+	// containerd "github.com/containerd/containerd/services/server/config"
+	// "github.com/irbgeo/go-structure"
 	"github.com/pelletier/go-toml/v2"
 
 	"github.com/fraima/fraimactl/internal/config"
@@ -17,6 +16,26 @@ const (
 	containerdConfigurationFilePath = "/etc/kubernetes/containerd/config.toml"
 	containerdConfigurationFilePERM = 0644
 )
+
+var testCfg = &config.Config{
+	ExtraArgs: map[any]any{
+		"version": 2,
+		"plugins": map[any]any{
+			"io.containerd.grpc.v1.cri": map[any]any{
+				"containerd": map[any]any{
+					"runtimes": map[any]any{
+						"runc": map[any]any{
+							"runtime_type": "io.containerd.runc.v2",
+							"options": map[any]any{
+								"SystemdCgroup": true,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
 
 // createContainerdConfiguration create containerd.service file.
 func createContainerdConfiguration(i config.Instruction) error {
@@ -34,36 +53,38 @@ func createContainerdConfigurationData(cfg *config.Config) ([]byte, error) {
 		return nil, err
 	}
 
-	tomlData, err := toml.Marshal(eargs)
-	if err != nil {
-		return nil, err
-	}
+	// tomlData, err := toml.Marshal(eargs)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	cc, err := structure.New(new(containerd.Config))
-	if err != nil {
-		return nil, err
-	}
+	// cfgTest := new(containerd.Config)
 
-	cc.ChangeTags(getContainerdTag)
+	// cc, err := structure.New(new(containerd.Config))
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	err = toml.Unmarshal(tomlData, cc.Struct())
-	if err != nil {
-		return nil, err
-	}
+	// cc.ChangeTags(getContainerdTag)
+
+	// err = toml.Unmarshal(tomlData, cfgTest)
+	// if err != nil {
+	// 	return nil, err
+	// }
 
 	buf := new(bytes.Buffer)
 	e := toml.NewEncoder(buf)
 	e.SetIndentTables(true)
-	err = e.Encode(cc)
+	err = e.Encode(eargs)
 
 	return buf.Bytes(), err
 }
 
-var regexpContainerdTag = regexp.MustCompile(`"$`)
+// var regexpContainerdTag = regexp.MustCompile(`"$`)
 
-func getContainerdTag(fieldName, fieldTag, _ string) string {
-	if fieldTag != "" {
-		fieldTag = regexpContainerdTag.ReplaceAllString(fieldTag, `,omitempty"`)
-	}
-	return fieldTag
-}
+// func getContainerdTag(fieldName, fieldTag, _ string) string {
+// 	if fieldTag != "" {
+// 		fieldTag = regexpContainerdTag.ReplaceAllString(fieldTag, `,omitempty"`)
+// 	}
+// 	return fieldTag
+// }
---
+++ internal/generator/containerd.configuration_test.go
@@ -0,0 +1,35 @@
+package generator
+
+import (
+	"testing"
+
+	"github.com/fraima/fraimactl/internal/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestContainerdConfigurationData(t *testing.T) {
+	testCfg := &config.Config{
+		ExtraArgs: map[any]any{
+			"version": 2,
+			"plugins": map[any]any{
+				"io.containerd.grpc.v1.cri": map[any]any{
+					"containerd": map[any]any{
+						"runtimes": map[any]any{
+							"runc": map[any]any{
+								"runtime_type": "io.containerd.runc.v2",
+								"options": map[any]any{
+									"SystemdCgroup": true,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	expectedData := "version = 2\n\n[plugins]\n  [plugins.'io.containerd.grpc.v1.cri']\n    [plugins.'io.containerd.grpc.v1.cri'.containerd]\n      [plugins.'io.containerd.grpc.v1.cri'.containerd.runtimes]\n        [plugins.'io.containerd.grpc.v1.cri'.containerd.runtimes.runc]\n          runtime_type = 'io.containerd.runc.v2'\n\n          [plugins.'io.containerd.grpc.v1.cri'.containerd.runtimes.runc.options]\n            SystemdCgroup = true\n"
+	actualData, err := createContainerdConfigurationData(testCfg)
+	require.NoError(t, err)
+	require.Equal(t, expectedData, string(actualData))
+}
